7eb8fd48f4f850769aa9b16fcd680ce8d1c5dc9b
Bruno Nascimento <bruno.hn@icloud.com>
2022-11-25 14:27:54 -0300

    docs: reproduction of the error when using previewData() with generateStaticParams

--- app/[[...route]]/page.tsx
+++ app/[[...route]]/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import { previewData } from "next/headers";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const preview = previewData();
+
+  console.log({ preview });
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -10,7 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>app/page.tsx</code>
         </p>
 
@@ -46,12 +51,28 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
+
+export const generateStaticParams = async () => {
+  const paths = [
+    {
+      route: [""],
+    },
+    {
+      route: ["about"],
+    },
+    {
+      route: ["services"],
+    },
+  ];
+
+  return paths;
+};
